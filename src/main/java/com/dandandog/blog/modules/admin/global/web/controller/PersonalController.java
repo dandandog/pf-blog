ae3d00d099eec4d8f883ae7826423b5f46da2fe9
JohnnyLiu <704365036@qq.com>
2021-09-29 18:33:41 +0800

    personal

--- src/main/java/com/dandandog/blog/modules/admin/global/web/controller/PersonalController.java
+++ src/main/java/com/dandandog/blog/modules/admin/global/web/controller/PersonalController.java
@@ -6,9 +6,14 @@ import com.dandandog.blog.modules.admin.global.web.faces.vo.UserInfoVo;
 import com.dandandog.framework.common.utils.SecurityUtil;
 import com.dandandog.framework.faces.controller.FacesController;
 import com.dandandog.framework.mapstruct.MapperUtil;
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.file.UploadedFile;
 import org.springframework.stereotype.Controller;
 
 import javax.annotation.Resource;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Optional;
 
 /**
@@ -28,10 +33,21 @@ public class PersonalController extends FacesController {
         AuthUser user = getCurrUser().orElse(null);
         UserInfoVo userVo = MapperUtil.map(user, UserInfoVo.class);
         putViewScope("user", userVo);
+        putViewScope("avatar", null);
     }
 
     public void save() {
         UserInfoVo vo = getViewScope("user");
+    }
+
+    public void handleFileUpload(FileUploadEvent event) throws IOException {
+        UploadedFile file = event.getFile();
+        InputStream inputStream = file.getInputStream();
+        DefaultStreamedContent content = DefaultStreamedContent.builder()
+                .contentType(file.getContentType())
+                .stream(() -> inputStream)
+                .build();
+        putViewScope("avatar", content);
     }
 
 

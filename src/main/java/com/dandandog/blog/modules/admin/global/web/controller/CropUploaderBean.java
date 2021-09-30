099fe1e71d8051c568b1249369e3604b65d4eed0
JohnnyLiu <704365036@qq.com>
2021-09-30 17:59:27 +0800

    头像

---
+++ src/main/java/com/dandandog/blog/modules/admin/global/web/controller/CropUploaderBean.java
@@ -0,0 +1,95 @@
+package com.dandandog.blog.modules.admin.global.web.controller;
+
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.CroppedImage;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
+import org.primefaces.model.file.UploadedFile;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+import java.io.ByteArrayInputStream;
+import java.io.Serializable;
+
+/**
+ * @Author: JohnnyLiu
+ * @Date: 2021/9/30 14:27
+ */
+@Component
+@SessionScope
+public class CropUploaderBean implements Serializable {
+
+    private CroppedImage croppedImage;
+
+    private UploadedFile originalImageFile;
+
+    public CroppedImage getCroppedImage() {
+        return croppedImage;
+    }
+
+    public void setCroppedImage(CroppedImage croppedImage) {
+        this.croppedImage = croppedImage;
+    }
+
+    public UploadedFile getOriginalImageFile() {
+        return originalImageFile;
+    }
+
+    public void handleFileUpload(FileUploadEvent event) {
+        this.originalImageFile = null;
+        this.croppedImage = null;
+        UploadedFile file = event.getFile();
+        if (file != null && file.getContent() != null && file.getContent().length > 0 && file.getFileName() != null) {
+            this.originalImageFile = file;
+
+        }
+    }
+
+    public void crop() {
+    }
+
+    public StreamedContent getImage() {
+        return DefaultStreamedContent.builder()
+                .contentType(originalImageFile == null ? null : originalImageFile.getContentType())
+                .stream(() -> {
+                    if (originalImageFile == null
+                            || originalImageFile.getContent() == null
+                            || originalImageFile.getContent().length == 0) {
+                        return null;
+                    }
+
+                    try {
+                        return new ByteArrayInputStream(originalImageFile.getContent());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        return null;
+                    }
+                })
+                .build();
+    }
+
+    public StreamedContent getCropped() {
+        return DefaultStreamedContent.builder()
+                .contentType(originalImageFile == null ? null : originalImageFile.getContentType())
+                .stream(() -> {
+                    if (croppedImage == null
+                            || croppedImage.getBytes() == null
+                            || croppedImage.getBytes().length == 0) {
+                        return null;
+                    }
+
+                    try {
+                        return new ByteArrayInputStream(this.croppedImage.getBytes());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        return null;
+                    }
+                })
+                .build();
+    }
+
+    public void clear() {
+        this.croppedImage = null;
+        this.originalImageFile = null;
+    }
+}
--- src/main/java/com/dandandog/blog/modules/admin/global/web/controller/PersonalController.java
+++ src/main/java/com/dandandog/blog/modules/admin/global/web/controller/PersonalController.java
@@ -2,10 +2,13 @@ package com.dandandog.blog.modules.admin.global.web.controller;
 
 import com.dandandog.blog.modules.admin.auth.entity.AuthUser;
 import com.dandandog.blog.modules.admin.auth.service.AuthUserService;
+import com.dandandog.blog.modules.admin.global.web.faces.vo.FileVo;
 import com.dandandog.blog.modules.admin.global.web.faces.vo.UserInfoVo;
 import com.dandandog.framework.common.utils.SecurityUtil;
 import com.dandandog.framework.faces.controller.FacesController;
 import com.dandandog.framework.mapstruct.MapperUtil;
+import com.dandandog.framework.mapstruct.qualifier.QualifierUrl;
+import com.dandandog.framework.oos.service.OosFileService;
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.CroppedImage;
 import org.primefaces.model.DefaultStreamedContent;
@@ -14,11 +17,8 @@ import org.primefaces.model.file.UploadedFile;
 import org.springframework.stereotype.Controller;
 
 import javax.annotation.Resource;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Optional;
 
 /**
@@ -29,9 +29,17 @@ import java.util.Optional;
 @Controller("/admin/global/personal.faces")
 public class PersonalController extends FacesController {
 
-
     @Resource
     private AuthUserService userService;
+
+    @Resource
+    private CropUploaderBean cropUploaderBean;
+
+    @Resource
+    private OosFileService oosFileService;
+
+    @Resource
+    private QualifierUrl qualifierUrl;
 
     @Override
     public void onEntry() {
@@ -42,47 +50,64 @@ public class PersonalController extends FacesController {
     }
 
     public void save() {
+        UploadedFile imgFile = cropUploaderBean.getOriginalImageFile();
+        String url = oosFileService.putItem(imgFile.getFileName(), cropUploaderBean.getCropped().getStream());
         UserInfoVo vo = getViewScope("user");
+        vo.setAvatarUrl(qualifierUrl.addDomain(url));
+        AuthUser user = MapperUtil.map(vo, AuthUser.class);
+        userService.saveOrUpdate(user);
+
     }
 
     public void edit() {
+        cropUploaderBean.clear();
         putViewScope("croppedImage", null);
-        putViewScope("originalImageFile", null);
-        putViewScope("originalImage", null);
+        putSessionScope("originalImageFile", null);
+        putSessionScope("originalImage", null);
     }
 
 
     public void handleFileUpload(FileUploadEvent event) throws IOException {
         UploadedFile file = event.getFile();
-        InputStream inputStream = file.getInputStream();
+        byte[] bytes = file.getContent();
         StreamedContent content = DefaultStreamedContent.builder()
                 .contentType(file.getContentType())
-                .stream(() -> inputStream)
+                .stream(() -> new ByteArrayInputStream(bytes))
                 .build();
-        putViewScope("originalImageFile", file);
-        putViewScope("originalImage", content);
+        putSessionScope("originalImageFile", new FileVo(file));
+        putSessionScope("originalImage", content);
     }
 
 
-    public StreamedContent getImage() throws IOException {
+    public StreamedContent getImage() {
         UploadedFile file = getViewScope("originalImageFile");
-        InputStream inputStream = file.getInputStream();
+        byte[] bytes = file.getContent();
         return DefaultStreamedContent.builder()
                 .contentType(file.getContentType())
-                .stream(() -> inputStream)
+                .stream(() -> new ByteArrayInputStream(bytes))
+                .build();
+    }
+
+    public StreamedContent getCropped() {
+        UploadedFile file = getViewScope("originalImageFile");
+        CroppedImage croppedImage = getViewScope("croppedImage");
+        byte[] bytes = croppedImage.getBytes();
+        return DefaultStreamedContent.builder()
+                .contentType(file.getContentType())
+                .stream(() -> new ByteArrayInputStream(bytes))
                 .build();
     }
 
 
     public void crop() {
         CroppedImage croppedImage = getViewScope("croppedImage");
-        UploadedFile file = getViewScope("originalImageFile");
+        FileVo vo = getViewScope("originalImageFile");
         byte[] bytes = croppedImage.getBytes();
         DefaultStreamedContent content = DefaultStreamedContent.builder()
-                .contentType(file.getContentType())
+                .contentType(vo.getFile().getContentType())
                 .stream(() -> new ByteArrayInputStream(bytes))
                 .build();
-        putViewScope("croppedImage", content);
+        putSessionScope("croppedImage", content);
     }
 
 
